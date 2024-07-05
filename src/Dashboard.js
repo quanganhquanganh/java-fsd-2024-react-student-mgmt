3d671ae422a81a8b242723d5fe4672550fb23816
quanganhquanganh <quanganhdb2510@gmail.com>
2024-07-05 11:48:57 +0700

    Change to Dashboard and UserProfile pages

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,43 +1,66 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Button, Table } from 'react-bootstrap';
-
+import React, { useContext } from 'react';
+import { Button, Table, Navbar, Nav, Container, Dropdown } from 'react-bootstrap';
+import { PersonCircle } from 'react-bootstrap-icons';
 import { UserContext } from './UserContext';
 
 export default function Dashboard() {
   const { user, users, switchDelete, logout } = useContext(UserContext);
 
   return (
-    <>
-      <h1>Welcome to Brolim student dashboard, {user}</h1>
-      <Button variant="primary" onClick={logout}>
-        Logout  
-      </Button>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Password</th>
-            <th>Is Admin</th>
-            <th>Soft Delete</th>
-            <th>Delete</th>
-          </tr> 
-        </thead>
-        <tbody>
-        {users.map(user => (
-          <tr key={user.username}>
-            <td>{user.username}</td>
-            <td>{user.password}</td>
-            <td>{user.isAdmin.toString()}</td>
-            <td>{user.softDelete.toString()}</td>
-            <td>
-              <Button variant="danger" onClick={() => switchDelete(user.username)}>
-                {user.softDelete ? 'Undelete' : 'Delete'}
-              </Button>
-            </td>
-          </tr>
-        ))}
-        </tbody>
-      </Table>
-    </>
-  )
+    <Container fluid className="p-0">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            <img alt="logo" src="brolimlogo.png" className="rounded-circle" width={"60"}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#users" active>Users</Nav.Link>
+              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+            </Nav>
+            <Dropdown align="end">
+              <Dropdown.Toggle variant="light" id="dropdown-basic">
+                <PersonCircle size={24} />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Header>{user}</Dropdown.Header>
+                <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-4">
+        <h1>Welcome to Brolim Admin Dashboard, {user}</h1>
+        <Table striped bordered hover className="mt-4">
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Password</th>
+              <th>Is Admin</th>
+              <th>Soft Delete</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.username}>
+                <td>{user.username}</td>
+                <td>{user.password}</td>
+                <td>{user.isAdmin.toString()}</td>
+                <td>{user.softDelete.toString()}</td>
+                <td>
+                  <Button variant="danger" size="sm" onClick={() => switchDelete(user.username)}>
+                    {user.softDelete ? 'Undelete' : 'Delete'}
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </Container>
+  );
 }
--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,16 +1,38 @@
-import React, { useContext } from "react";
+import React from "react";
+import { Navbar, Nav, Container, Dropdown } from 'react-bootstrap';
+import { PersonCircle } from 'react-bootstrap-icons';
+import { useContext } from "react";
 import { UserContext } from "./UserContext";
-import { Button } from "react-bootstrap";
 
 export default function UserProfile() {
-    const { user, logout } = useContext(UserContext);
-    return (
-      <>
-        <h1>Welcome to Brolim, {user}</h1>
-        <p>You have finished all your courses.</p>
-        <Button variant="primary" onClick={logout}>
-          Logout
-        </Button>
-      </>
-    )
+  const { user, logout } = useContext(UserContext);
+
+  return (
+    <Container fluid className="p-0">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            <img alt="logo" src="brolimlogo.png" className="rounded-circle" width={"60"}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#learning-paths" active>Learning Path(s)</Nav.Link>
+              <Nav.Link href="#courses">Course(s)</Nav.Link>
+            </Nav>
+            <Dropdown align="end">
+              <Dropdown.Toggle variant="light" id="dropdown-basic">
+                <PersonCircle size={24} />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Header>{user}</Dropdown.Header>
+                <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
+  );
 }
